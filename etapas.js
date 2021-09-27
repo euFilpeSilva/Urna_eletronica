cd9871d6fa7d1ab8ca390ec7bdb619e08c0be5c4
Filipe Silva <sousadasilvafilipe@gmail.com>
2021-09-26 21:56:45 -0300

    implementando o js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo:'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+      numero: 38111,
+      nome:'Fulano de tal',
+      partido:'ABC',
+      fotos: [
+          {url:'prefeito1.jpg', legenda: 'Vereador'}
+      ]
+      },
+      {
+        numero:77222,
+        nome: 'Beltrano da Silva',
+        partido: 'DEFG',
+        fotos:[
+          {url:'prefeito3.png', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+   titulo: 'PREFEITO',
+   numeros: 2,
+   candidatos: [
+     {
+       numero:99,
+       nome: 'Ciclano',
+       partido:'ABC',
+       vice: 'Cic',
+       fotos:[
+         {url:'prefeito1.jpg', legenda: 'Prefeito'},
+         {url:'vice-prefeito1.jpg', legenda: 'Vice-Prefeito', small: true}
+       ]
+     },
+     {
+      numero:84,
+      nome: 'Zulano',
+      partido:'QWERTY',
+      vice: 'Zul',
+      fotos:[
+        {url:'prefeito2.jpg', legenda: 'Prefeito'},
+        {url:'vice-prefeito2.jpg', legenda: 'Vice-Prefeito', small: true}
+      ]
+     },
+   ]
+  }
+];
---
+++ script.js
@@ -0,0 +1,3 @@
+let seuVotoPara = document.querySelector('.divisao-1-1 span');
+let cargo = document.querySelector('.divisao-1-2 span');
+let descricao = document.querySelector('.divisao-1-4');
